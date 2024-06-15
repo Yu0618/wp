6c099870b6d9fff7819173d3e931144c9f363be1
Yu0618 <161789081+Yu0618@users.noreply.github.com>
2024-06-16 05:23:22 +0800

    Create p3.js

---
+++ html/hw3/p3.js
@@ -0,0 +1,19 @@
+//寫一個函數判斷 n 是否為質數 isPrime(n)
+function isprime(n) 
+{
+    
+    if (n <= 1)
+    {
+        return false;
+    }
+    for (var i = 2; i < n; i++) 
+    {
+        if(n % i == 0)
+        {
+            return false;
+        }
+    }
+    return true;
+}
+
+console.log(isprime(3));
